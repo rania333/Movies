cacc990ef34663e7538cdd9dfcd4c2f5a39672b2
rania333 <raniaaramadan1999@gmail.com>
2022-01-19 08:49:54 +0200

    add no favourite

--- src/component/Fav/Fav.jsx
+++ src/component/Fav/Fav.jsx
@@ -12,29 +12,32 @@ export default function Fav() {
     return (
         <div className="container">
         <div className="row">
-                {mySelector.map((movie, index) => {
-                    let pic = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
-                    return (
-                    <div className="col-3 py-2" key={index}>
-                        <div className="card" style={{width: '18rem'}}>
-                            <img src={pic} className="card-img-top" style={{ height: '15rem'}}/>
-                            <div className="card-body">
-                                <h5 className="card-title">{movie.title}</h5>
-                                <p className="card-text">
-                                    {movie.vote_count}
-                                </p>
-                                <h2>
-                                    <i onClick={(e) => removeFromFav(e, movie)} 
-                                    className="bi bi-heart-fill text-danger"></i>
-                                </h2>
-                                <Link to={`/details/${movie.id}`} 
-                                className="btn btn-info">
-                                    More Details</Link>
-                            </div>
+            {mySelector.length>0 ? 
+            mySelector.map((movie, index) => {
+                let pic = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+                return (
+                <div className="col-3 py-2" key={index}>
+                    <div className="card" style={{width: '18rem'}}>
+                        <img src={pic} className="card-img-top" style={{ height: '15rem'}}/>
+                        <div className="card-body">
+                            <h5 className="card-title">{movie.title}</h5>
+                            <p className="card-text">
+                                {movie.vote_count}
+                            </p>
+                            <h2>
+                                <i onClick={(e) => removeFromFav(e, movie)} 
+                                className="bi bi-heart-fill text-danger"></i>
+                            </h2>
+                            <Link to={`/details/${movie.id}`} 
+                            className="btn btn-info">
+                                More Details</Link>
                         </div>
                     </div>
-                    )
-                })}
+                </div>
+                )
+            }): <h1 className='mx-auto my-5 text-info'>
+                You hadn't add any movie to favourites yet
+                </h1>} 
             </div>
             </div>
     )
